7363c8170174c84f230cd325715b4b9a94125d79
Ankit3060 <ankit330660@gmail.com>
2024-06-17 16:42:10 +0530

    Add new doctor and admin dashboard done

--- dashboard/src/components/AddNewAdmin.jsx
+++ dashboard/src/components/AddNewAdmin.jsx
@@ -1,7 +1,8 @@
 import React, { useContext, useState } from 'react'
 import { Context } from '../main';
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 function AddNewAdmin() {
   const [firstName,setFirstName] = useState("");
@@ -13,59 +14,111 @@ function AddNewAdmin() {
   const [gender, setGender] = useState("");
   const [password,setPassword] = useState("");
 
-  const {isAuthenticated,setIsAuthenticated} =  useContext(Context)
+  const {isAuthenticated} =  useContext(Context)
   const navigateTo = useNavigate();
 
-  const handleRegister = async (e) => {
+  const handleAddNewAdmin = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post(
         "http://localhost:4000/api/v1/user/admin/addnew",
-
-      )
+        {firstName,lastName,email,phone,pan,dob,gender,password},
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          }
+        }
+      );
+      toast.success(response.data.message);
+      navigateTo("/")
     } catch (error) {
+      toast.error(error.response.data.message);
       
     }
   }
-
-  // const handleRegister = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     const response =await axios.post(
-  //       "http://localhost:4000/api/v1/user/patient/register",
-  //       {firstName,lastName,email,phone,pan,dob,gender,password,role:"Patient"},
-  //       {
-  //         withCredentials: true,
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //       }
-  //     )
-  //     .then((res) => {
-  //       toast.success(res.data.message);
-  //       setIsAuthenticated(true);
-  //       navigateTo("/");
-  //       setFirstName("");
-  //       setLastName("");
-  //       setEmail("");
-  //       setPhone("");
-  //       setPan("");
-  //       setDob("");
-  //       setGender("");
-  //       setPassword("");
-  //     });
-  //   } catch (error) {
-  //     toast.error(error.response.data.message);
-  //   }
-  // }
-
-  // if(isAuthenticated){
-  //   return <Navigate to={"/"} />
-  // }
+  if(!isAuthenticated){
+    return <Navigate to={"/login"} />
+  }
 
 
   return (
     <>
+    <section className="page">
+    <div className='container form-component add-admin-form'>
+    <img src="/logo.png" alt="logo" className='logo' />
+      <h1 className='form-title'>Add New Admin</h1>
+      <form onSubmit={handleAddNewAdmin}>
+        <div>
+        <input 
+              type="text" 
+              placeholder='First Name' 
+              value={firstName}
+              onChange={(e)=>setFirstName(e.target.value)} 
+        />
+        <input 
+              type="text" 
+              placeholder='Last Name' 
+              value={lastName}
+              onChange={(e)=>setLastName(e.target.value)} 
+        />
+        </div>
+
+        <div>
+        <input 
+              type="text" 
+              placeholder='Email' 
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)} 
+        />
+        <input 
+              type="tel" 
+              placeholder='Phone' 
+              value={phone}
+              onChange={(e)=>setPhone(e.target.value)} 
+        />
+        </div>
+
+        <div>
+        <input 
+              type="text" 
+              placeholder='PAN' 
+              value={pan}
+              onChange={(e)=>setPan(e.target.value)} 
+        />
+        <input 
+              type="date" 
+              placeholder='DOB' 
+              value={dob}
+              onChange={(e)=>setDob(e.target.value)} 
+        />
+        </div>
+
+        <div>
+        <select value={gender} onChange={(e)=>setGender(e.target.value)}>
+          <option value="">Gender</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+
+        <input 
+              type="password" 
+              placeholder='Password' 
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)} 
+        />
+        </div>
+
+        {/* Register button */}
+        <div style={{justifyContent:"center" , alignItems:"center"}}>
+          <button type="submit">Add New Admin</button>
+        </div>
+
+      </form>
+    </div>
+
+
+    </section>
     </>
   )
 }
--- dashboard/src/components/AddNewDoctor.jsx
+++ dashboard/src/components/AddNewDoctor.jsx
@@ -1,8 +1,167 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { Context } from '../main';
+import { Navigate, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 function AddNewDoctor() {
+  const [firstName,setFirstName] = useState("");
+  const [lastName,setLastName] = useState("");
+  const [email,setEmail] = useState("");
+  const [phone,setPhone] = useState("");
+  const [pan,setPan] = useState("");
+  const [dob, setDob] = useState("");
+  const [gender, setGender] = useState("");
+  const [password,setPassword] = useState("");
+  const [doctorDepartment, setDoctorDepartment] = useState("");
+  const [docAvatar, setDocAvatar] = useState("");
+  const [docAvatarPreview, setDocAvatarPreview] = useState("");
+
+  const {isAuthenticated} =  useContext(Context)
+  const navigateTo = useNavigate();
+
+  //handling the avatar
+  const handleAvatar = async (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      setDocAvatarPreview(reader.result);
+      setDocAvatar(file);
+    }
+  }
+
+  const handleAddNewDoctor = async (e) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData();  // FormData is used to send files to the server
+      formData.append("firstName",firstName);
+      formData.append("lastName",lastName);
+      formData.append("email",email);
+      formData.append("phone",phone);
+      formData.append("pan",pan);
+      formData.append("dob",dob);
+      formData.append("gender",gender);
+      formData.append("password",password);
+      formData.append("doctorDepartment",doctorDepartment);
+      formData.append("docAvatar",docAvatar);
+
+      const response = await axios.post(
+        "http://localhost:4000/api/v1/user/doctor/addnew",
+        formData,
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "multipart/form-data",
+          }
+        }
+      );
+      toast.success(response.data.message);
+      navigateTo("/")
+    } catch (error) {
+      toast.error(error.response.data.message);
+      
+    }
+  }
+  if(!isAuthenticated){
+    return <Navigate to={"/login"} />
+  }
+
+  const departmentArray = [
+    "Pediatrics",
+    "Cardiology",
+    "Neurologist",
+    "Orthopedics",
+    "Dermatology",
+    "Gynecology",
+    "Ophthalmology",
+    "ENT",
+    "Urology",
+    "Radiology",
+    "Dentist",
+    "Physiotherapy",
+];
+
   return (
-    <div>AddNewDoctor</div>
+    <>
+    <section className="page">
+    <div className='container form-component add-doctor-form'>
+    <img src="/logo.png" alt="logo" className='logo' />
+      <h1 className='form-title'>Register New Doctor</h1>
+      <form onSubmit={handleAddNewDoctor}>
+
+      <div className='first-wrapper'>
+        {/* Avatar */}
+        <div style={{flexDirection:"column",height:"fit-content"}}>
+          <img src={docAvatarPreview ? `${docAvatarPreview}` : "/docHolder.jpg"} alt="Doctor Avatar" />
+          <input type="file" onChange={handleAvatar} />
+        </div>
+
+        <div>
+        <input 
+              type="text" 
+              placeholder='First Name' 
+              value={firstName}
+              onChange={(e)=>setFirstName(e.target.value)} 
+        />
+        <input 
+              type="text" 
+              placeholder='Last Name' 
+              value={lastName}
+              onChange={(e)=>setLastName(e.target.value)} 
+        />
+        <input 
+              type="text" 
+              placeholder='Email' 
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)} 
+        />
+        <input 
+              type="tel" 
+              placeholder='Phone' 
+              value={phone}
+              onChange={(e)=>setPhone(e.target.value)} 
+        />
+        <input 
+              type="text" 
+              placeholder='PAN' 
+              value={pan}
+              onChange={(e)=>setPan(e.target.value)} 
+        />
+        <input 
+              type="date" 
+              placeholder='DOB' 
+              value={dob}
+              onChange={(e)=>setDob(e.target.value)} 
+        />
+        <select value={gender} onChange={(e)=>setGender(e.target.value)}>
+          <option value="">Gender</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+        <input 
+              type="password" 
+              placeholder='Password' 
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)} 
+        />
+        <select value={doctorDepartment} onChange={(e)=>setDoctorDepartment(e.target.value)}>
+          <option value="">Select Department</option>
+                {departmentArray.map((depart,index)=>{
+                    return(
+                        <option key={index} value={depart}>
+                            {depart}
+                        </option>
+                    )
+                })}
+        </select>
+        <button type="submit">Register New Doctor</button>
+        </div>
+      </div>
+      </form>
+    </div>
+    </section>
+    </>
   )
 }
 
